735345b7627912068296bcc55532af5a3a31eaec
philcrick <phil.crick@anglicaresa.com.au>
2017-07-19 08:53:50 +0930

    added finance tables

---
+++ ReportingWarehouse/SQL_Scripts/PROD/dwhb/management/dimension/populate_finance_table_dim.sql
@@ -0,0 +1,33 @@
+﻿-------------------
+-- DEV environment
+-------------------
+
+-- schema: t1 source tables
+-- none at the moment (uses existing T1 tables from HRIS)
+
+-- schema: extracted t1 tables
+-- none at the moment (uses existing T1 tables from HRIS)
+
+-- schema: conform_finance tables
+INSERT INTO [management].[TABLE_DIM] VALUES ('account_dim', 'dwh', 'conform_finance', 'dwhb', 'AZ-SQLBI01');
+INSERT INTO [management].[TABLE_DIM] VALUES ('accounting_period_dim', 'dwh', 'conform_finance', 'dwhb', 'AZ-SQLBI01');
+INSERT INTO [management].[TABLE_DIM] VALUES ('cost_centre_dim', 'dwh', 'conform_finance', 'dwhb', 'AZ-SQLBI01');
+INSERT INTO [management].[TABLE_DIM] VALUES ('date_dim', 'dwh', 'conform_finance', 'dwhb', 'AZ-SQLBI01');
+INSERT INTO [management].[TABLE_DIM] VALUES ('ledger_dim', 'dwh', 'conform_finance', 'dwhb', 'AZ-SQLBI01');
+INSERT INTO [management].[TABLE_DIM] VALUES ('portfolio_dim', 'dwh', 'conform_finance', 'dwhb', 'AZ-SQLBI01');
+INSERT INTO [management].[TABLE_DIM] VALUES ('program_dim', 'dwh', 'conform_finance', 'dwhb', 'AZ-SQLBI01');
+INSERT INTO [management].[TABLE_DIM] VALUES ('service_stream_dim', 'dwh', 'conform_finance', 'dwhb', 'AZ-SQLBI01');
+
+INSERT INTO [management].[TABLE_DIM] VALUES ('debtor_fact', 'dwh', 'conform_finance', 'dwhb', 'AZ-SQLBI01');
+
+-- schema: finance tables
+INSERT INTO [management].[TABLE_DIM] VALUES ('account_dim', 'dwh', 'finance', 'dwhb', 'AZ-SQLBI01');
+INSERT INTO [management].[TABLE_DIM] VALUES ('accounting_period_dim', 'dwh', 'finance', 'dwhb', 'AZ-SQLBI01');
+INSERT INTO [management].[TABLE_DIM] VALUES ('cost_centre_dim', 'dwh', 'finance', 'dwhb', 'AZ-SQLBI01');
+INSERT INTO [management].[TABLE_DIM] VALUES ('date_dim', 'dwh', 'finance', 'dwhb', 'AZ-SQLBI01');
+INSERT INTO [management].[TABLE_DIM] VALUES ('ledger_dim', 'dwh', 'finance', 'dwhb', 'AZ-SQLBI01');
+INSERT INTO [management].[TABLE_DIM] VALUES ('portfolio_dim', 'dwh', 'finance', 'dwhb', 'AZ-SQLBI01');
+INSERT INTO [management].[TABLE_DIM] VALUES ('program_dim', 'dwh', 'finance', 'dwhb', 'AZ-SQLBI01');
+INSERT INTO [management].[TABLE_DIM] VALUES ('service_stream_dim', 'dwh', 'finance', 'dwhb', 'AZ-SQLBI01');
+
+INSERT INTO [management].[TABLE_DIM] VALUES ('debtor_fact', 'dwh', 'finance', 'dwhb', 'AZ-SQLBI01');
