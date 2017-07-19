e22b1e98af8c29c08fcd0a59ca876d57a8788347
frank.wang <frank.wang@anglicaresa.com.au>
2017-07-19 11:43:05 +0930

    create_debtor_fact.sql added.

--- ReportingWarehouse/SQL_Scripts/COMMON/dwhb/conform_finance2/fact/create_debtor_fact.sql
+++ ReportingWarehouse/SQL_Scripts/COMMON/dwhb/conform_finance2/fact/create_debtor_fact.sql
@@ -19,3 +19,76 @@ alter table [conform_finance].[debtor_fact]
 add constraint [pk_debtor_fact]
     primary key clustered ([debtor_key] asc);
 go
+
+-- creating foreign key on [account_key] in table [DEBTOR_FACT]
+alter table [conform_finance].[DEBTOR_FACT]
+add constraint [fk_conform_finance_DEBTOR_FACT_account_key]
+    foreign key ([account_key])
+    references [conform_finance].[account_dim]
+        (account_key)
+    on delete no action on update no action;
+go
+-- creating foreign key on [cost_centre_key] in table [DEBTOR_FACT]
+alter table [conform_finance].[DEBTOR_FACT]
+add constraint [fk_conform_finance_DEBTOR_FACT_cost_centre_key]
+    foreign key ([cost_centre_key])
+    references [conform_finance].[cost_centre_dim]
+        (cost_centre_key)
+    on delete no action on update no action;
+go
+-- creating foreign key on [portfolio_key] in table [DEBTOR_FACT]
+alter table [conform_finance].[DEBTOR_FACT]
+add constraint [fk_conform_finance_DEBTOR_FACT_portfolio_key]
+    foreign key ([portfolio_key])
+    references [conform_finance].[portfolio_dim]
+        (portfolio_key)
+    on delete no action on update no action;
+go
+-- creating foreign key on [service_stream_key] in table [DEBTOR_FACT]
+alter table [conform_finance].[DEBTOR_FACT]
+add constraint [fk_conform_finance_DEBTOR_FACT_service_stream_key]
+    foreign key ([service_stream_key])
+    references [conform_finance].[service_stream_dim]
+        (service_stream_key)
+    on delete no action on update no action;
+go
+-- creating foreign key on [program_key] in table [DEBTOR_FACT]
+alter table [conform_finance].[DEBTOR_FACT]
+add constraint [fk_conform_finance_DEBTOR_FACT_program_key]
+    foreign key ([program_key])
+    references [conform_finance].[program_dim]
+        (program_key)
+    on delete no action on update no action;
+go
+-- creating foreign key on [accounting_period_key] in table [DEBTOR_FACT]
+alter table [conform_finance].[DEBTOR_FACT]
+add constraint [fk_conform_finance_DEBTOR_FACT_accounting_period_key]
+    foreign key ([accounting_period_key])
+    references [conform_finance].[accounting_period_dim]
+        (accounting_period_key)
+    on delete no action on update no action;
+go
+-- creating foreign key on [date_received_key] in table [DEBTOR_FACT]
+alter table [conform_finance].[DEBTOR_FACT]
+add constraint [fk_conform_finance_DEBTOR_FACT_date_received_key]
+    foreign key ([date_received_key])
+    references [conform_finance].[date_dim]
+        (date_key)
+    on delete no action on update no action;
+go
+-- creating foreign key on [date_processed_key] in table [DEBTOR_FACT]
+alter table [conform_finance].[DEBTOR_FACT]
+add constraint [fk_conform_finance_DEBTOR_FACT_date_processed_key]
+    foreign key ([date_processed_key])
+    references [conform_finance].[date_dim]
+        (date_key)
+    on delete no action on update no action;
+go
+-- creating foreign key on [ledger_key] in table [DEBTOR_FACT]
+alter table [conform_finance].[DEBTOR_FACT]
+add constraint [fk_conform_finance_DEBTOR_FACT_ledger_key]
+    foreign key ([ledger_key])
+    references [conform_finance].[ledger_dim]
+        (ledger_key)
+    on delete no action on update no action;
+go
