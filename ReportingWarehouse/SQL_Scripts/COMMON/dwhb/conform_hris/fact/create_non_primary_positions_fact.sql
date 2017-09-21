5d7194562d7c9f4f05736b6bceb99c0a61d30fc6
frank.wang <frank.wang@anglicaresa.com.au>
2017-09-21 15:18:34 +0930

    Pay_rate added to Non primary position fact table.

--- ReportingWarehouse/SQL_Scripts/COMMON/dwhb/conform_hris/fact/create_non_primary_positions_fact.sql
+++ ReportingWarehouse/SQL_Scripts/COMMON/dwhb/conform_hris/fact/create_non_primary_positions_fact.sql
@@ -1,112 +1,138 @@
 USE [DWHB]
 GO
+
+ALTER TABLE [conform].[non_primary_positions_fact] DROP CONSTRAINT [fk_conform_non_primary_positions_fact_service_stream_key]
+GO
+
+ALTER TABLE [conform].[non_primary_positions_fact] DROP CONSTRAINT [fk_conform_non_primary_positions_fact_program_key]
+GO
+
+ALTER TABLE [conform].[non_primary_positions_fact] DROP CONSTRAINT [fk_conform_non_primary_positions_fact_position_key]
+GO
+
+ALTER TABLE [conform].[non_primary_positions_fact] DROP CONSTRAINT [fk_conform_non_primary_positions_fact_position_end_date_key]
+GO
+
+ALTER TABLE [conform].[non_primary_positions_fact] DROP CONSTRAINT [fk_conform_non_primary_positions_fact_position_effective_date_key]
+GO
+
+ALTER TABLE [conform].[non_primary_positions_fact] DROP CONSTRAINT [fk_conform_non_primary_positions_fact_portfolio_key]
+GO
+
+ALTER TABLE [conform].[non_primary_positions_fact] DROP CONSTRAINT [fk_conform_non_primary_positions_fact_manager_position_key]
+GO
+
+ALTER TABLE [conform].[non_primary_positions_fact] DROP CONSTRAINT [fk_conform_non_primary_positions_fact_manager_key]
+GO
+
+ALTER TABLE [conform].[non_primary_positions_fact] DROP CONSTRAINT [fk_conform_non_primary_positions_fact_employee_key]
+GO
+
+ALTER TABLE [conform].[non_primary_positions_fact] DROP CONSTRAINT [fk_conform_non_primary_positions_fact_contract_expire_date_key]
+GO
+
+/****** Object:  Table [conform].[non_primary_positions_fact]    Script Date: 21/09/2017 3:14:30 PM ******/
 DROP TABLE [conform].[non_primary_positions_fact]
 GO
+
+/****** Object:  Table [conform].[non_primary_positions_fact]    Script Date: 21/09/2017 3:14:30 PM ******/
 SET ANSI_NULLS ON
 GO
+
 SET QUOTED_IDENTIFIER ON
 GO
+
 CREATE TABLE [conform].[non_primary_positions_fact](
-   [non_primary_position_key] [int]   identity(1,1) NOT NULL,
-   [portfolio_key] [int] NOT NULL,
-   [service_stream_key] [int] NOT NULL,
-   [program_key] [int] NOT NULL,
-   [position_key] [int] NOT NULL,
-   [employee_key] [int] NOT NULL,
-   [position_type] [varchar] (100) NULL,
-   [position_code] [varchar] (100) NULL,
-   [manager_key] [int] NOT NULL,
-   [manager_position_key] [int] NOT NULL,
-   [contract_expire_date_key] [int]  NULL,
-   [position_effective_date_key] [int]  NULL,
-   [position_end_date_key] [int]  NULL
-)
-GO
-alter table [conform].[non_primary_positions_fact]
-add constraint [pk_non_primary_positions_fact]
-    primary key clustered([non_primary_position_key] asc);
-GO
--- creating foreign key on [portfolio_key] in table [non_primary_positions_fact]
-alter table [conform].[non_primary_positions_fact]
-add constraint [fk_conform_non_primary_positions_fact_portfolio_key]
-    foreign key ([portfolio_key])
-    references [conform].[portfolio_dim]
-        (portfolio_key)
-    on delete no action on update no action;
-go
--- creating foreign key on [program_key] in table [non_primary_positions_fact]
-alter table [conform].[non_primary_positions_fact]
-add constraint [fk_conform_non_primary_positions_fact_program_key]
-    foreign key ([program_key])
-    references [conform].[program_dim]
-        (program_key)
-    on delete no action on update no action;
-go
-
-alter table [conform].[non_primary_positions_fact]
-add constraint [fk_conform_non_primary_positions_fact_service_stream_key]
-    foreign key ([service_stream_key])
-    references [conform].[service_stream_dim]
-        (service_stream_key)
-    on delete no action on update no action;
-go
-
-alter table [conform].[non_primary_positions_fact]
-add constraint [fk_conform_non_primary_positions_fact_position_key]
-    foreign key ([position_key])
-    references [conform].[position_dim]
-        (position_key)
-    on delete no action on update no action;
-go
-
-alter table [conform].[non_primary_positions_fact]
-add constraint [fk_conform_non_primary_positions_fact_manager_position_key]
-    foreign key ([manager_position_key])
-    references [conform].[position_dim]
-        (position_key)
-    on delete no action on update no action;
-go
-
-
-alter table [conform].[non_primary_positions_fact]
-add constraint [fk_conform_non_primary_positions_fact_employee_key]
-    foreign key ([employee_key])
-    references [conform].[employee_dim]
-        (employee_key)
-    on delete no action on update no action;
-go
-
-alter table [conform].[non_primary_positions_fact]
-add constraint [fk_conform_non_primary_positions_fact_manager_key]
-    foreign key ([manager_key])
-    references [conform].[employee_dim]
-        (employee_key)
-    on delete no action on update no action;
-go
-
-alter table [conform].[non_primary_positions_fact]
-add constraint [fk_conform_non_primary_positions_fact_contract_expire_date_key]
-    foreign key ([contract_expire_date_key])
-    references [conform].[date_dim]
-        (date_key)
-    on delete no action on update no action;
-go
-
-alter table [conform].[non_primary_positions_fact]
-add constraint [fk_conform_non_primary_positions_fact_position_effective_date_key]
-    foreign key ([position_effective_date_key])
-    references [conform].[date_dim]
-        (date_key)
-    on delete no action on update no action;
-go
-
-alter table [conform].[non_primary_positions_fact]
-add constraint [fk_conform_non_primary_positions_fact_position_end_date_key]
-    foreign key ([position_end_date_key])
-    references [conform].[date_dim]
-        (date_key)
-    on delete no action on update no action;
-go
-
+	[non_primary_position_key] [int] IDENTITY(1,1) NOT NULL,
+	[portfolio_key] [int] NOT NULL,
+	[service_stream_key] [int] NOT NULL,
+	[program_key] [int] NOT NULL,
+	[position_key] [int] NOT NULL,
+	[employee_key] [int] NOT NULL,
+	[position_type] [varchar](100) NULL,
+	[position_code] [varchar](100) NULL,
+	[manager_key] [int] NOT NULL,
+	[manager_position_key] [int] NOT NULL,
+	[contract_expire_date_key] [int] NULL,
+	[position_effective_date_key] [int] NULL,
+	[position_end_date_key] [int] NULL,
+	[pay_rate] [decimal] (18,6) NULL,
+ CONSTRAINT [pk_non_primary_positions_fact] PRIMARY KEY CLUSTERED 
+(
+	[non_primary_position_key] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
 
 GO
+
+ALTER TABLE [conform].[non_primary_positions_fact]  WITH CHECK ADD  CONSTRAINT [fk_conform_non_primary_positions_fact_contract_expire_date_key] FOREIGN KEY([contract_expire_date_key])
+REFERENCES [conform].[date_dim] ([date_key])
+GO
+
+ALTER TABLE [conform].[non_primary_positions_fact] CHECK CONSTRAINT [fk_conform_non_primary_positions_fact_contract_expire_date_key]
+GO
+
+ALTER TABLE [conform].[non_primary_positions_fact]  WITH CHECK ADD  CONSTRAINT [fk_conform_non_primary_positions_fact_employee_key] FOREIGN KEY([employee_key])
+REFERENCES [conform].[employee_dim] ([employee_key])
+GO
+
+ALTER TABLE [conform].[non_primary_positions_fact] CHECK CONSTRAINT [fk_conform_non_primary_positions_fact_employee_key]
+GO
+
+ALTER TABLE [conform].[non_primary_positions_fact]  WITH CHECK ADD  CONSTRAINT [fk_conform_non_primary_positions_fact_manager_key] FOREIGN KEY([manager_key])
+REFERENCES [conform].[employee_dim] ([employee_key])
+GO
+
+ALTER TABLE [conform].[non_primary_positions_fact] CHECK CONSTRAINT [fk_conform_non_primary_positions_fact_manager_key]
+GO
+
+ALTER TABLE [conform].[non_primary_positions_fact]  WITH CHECK ADD  CONSTRAINT [fk_conform_non_primary_positions_fact_manager_position_key] FOREIGN KEY([manager_position_key])
+REFERENCES [conform].[position_dim] ([position_key])
+GO
+
+ALTER TABLE [conform].[non_primary_positions_fact] CHECK CONSTRAINT [fk_conform_non_primary_positions_fact_manager_position_key]
+GO
+
+ALTER TABLE [conform].[non_primary_positions_fact]  WITH CHECK ADD  CONSTRAINT [fk_conform_non_primary_positions_fact_portfolio_key] FOREIGN KEY([portfolio_key])
+REFERENCES [conform].[portfolio_dim] ([portfolio_key])
+GO
+
+ALTER TABLE [conform].[non_primary_positions_fact] CHECK CONSTRAINT [fk_conform_non_primary_positions_fact_portfolio_key]
+GO
+
+ALTER TABLE [conform].[non_primary_positions_fact]  WITH CHECK ADD  CONSTRAINT [fk_conform_non_primary_positions_fact_position_effective_date_key] FOREIGN KEY([position_effective_date_key])
+REFERENCES [conform].[date_dim] ([date_key])
+GO
+
+ALTER TABLE [conform].[non_primary_positions_fact] CHECK CONSTRAINT [fk_conform_non_primary_positions_fact_position_effective_date_key]
+GO
+
+ALTER TABLE [conform].[non_primary_positions_fact]  WITH CHECK ADD  CONSTRAINT [fk_conform_non_primary_positions_fact_position_end_date_key] FOREIGN KEY([position_end_date_key])
+REFERENCES [conform].[date_dim] ([date_key])
+GO
+
+ALTER TABLE [conform].[non_primary_positions_fact] CHECK CONSTRAINT [fk_conform_non_primary_positions_fact_position_end_date_key]
+GO
+
+ALTER TABLE [conform].[non_primary_positions_fact]  WITH CHECK ADD  CONSTRAINT [fk_conform_non_primary_positions_fact_position_key] FOREIGN KEY([position_key])
+REFERENCES [conform].[position_dim] ([position_key])
+GO
+
+ALTER TABLE [conform].[non_primary_positions_fact] CHECK CONSTRAINT [fk_conform_non_primary_positions_fact_position_key]
+GO
+
+ALTER TABLE [conform].[non_primary_positions_fact]  WITH CHECK ADD  CONSTRAINT [fk_conform_non_primary_positions_fact_program_key] FOREIGN KEY([program_key])
+REFERENCES [conform].[program_dim] ([program_key])
+GO
+
+ALTER TABLE [conform].[non_primary_positions_fact] CHECK CONSTRAINT [fk_conform_non_primary_positions_fact_program_key]
+GO
+
+ALTER TABLE [conform].[non_primary_positions_fact]  WITH CHECK ADD  CONSTRAINT [fk_conform_non_primary_positions_fact_service_stream_key] FOREIGN KEY([service_stream_key])
+REFERENCES [conform].[service_stream_dim] ([service_stream_key])
+GO
+
+ALTER TABLE [conform].[non_primary_positions_fact] CHECK CONSTRAINT [fk_conform_non_primary_positions_fact_service_stream_key]
+GO
+
+
--- ReportingWarehouse/SQL_Scripts/COMMON/dwhf_hris/fact/create_non_primary_positions_fact.sql
+++ ReportingWarehouse/SQL_Scripts/COMMON/dwhf_hris/fact/create_non_primary_positions_fact.sql
@@ -1,112 +1,138 @@
-USE [dwhf]
+USE [DWHF]
 GO
+
+ALTER TABLE [hris].[non_primary_positions_fact] DROP CONSTRAINT [fk_conform_non_primary_positions_fact_service_stream_key]
+GO
+
+ALTER TABLE [hris].[non_primary_positions_fact] DROP CONSTRAINT [fk_conform_non_primary_positions_fact_program_key]
+GO
+
+ALTER TABLE [hris].[non_primary_positions_fact] DROP CONSTRAINT [fk_conform_non_primary_positions_fact_position_key]
+GO
+
+ALTER TABLE [hris].[non_primary_positions_fact] DROP CONSTRAINT [fk_conform_non_primary_positions_fact_position_end_date_key]
+GO
+
+ALTER TABLE [hris].[non_primary_positions_fact] DROP CONSTRAINT [fk_conform_non_primary_positions_fact_position_effective_date_key]
+GO
+
+ALTER TABLE [hris].[non_primary_positions_fact] DROP CONSTRAINT [fk_conform_non_primary_positions_fact_portfolio_key]
+GO
+
+ALTER TABLE [hris].[non_primary_positions_fact] DROP CONSTRAINT [fk_conform_non_primary_positions_fact_manager_position_key]
+GO
+
+ALTER TABLE [hris].[non_primary_positions_fact] DROP CONSTRAINT [fk_conform_non_primary_positions_fact_manager_key]
+GO
+
+ALTER TABLE [hris].[non_primary_positions_fact] DROP CONSTRAINT [fk_conform_non_primary_positions_fact_employee_key]
+GO
+
+ALTER TABLE [hris].[non_primary_positions_fact] DROP CONSTRAINT [fk_conform_non_primary_positions_fact_contract_expire_date_key]
+GO
+
+/****** Object:  Table [hris].[non_primary_positions_fact]    Script Date: 21/09/2017 3:16:27 PM ******/
 DROP TABLE [hris].[non_primary_positions_fact]
 GO
+
+/****** Object:  Table [hris].[non_primary_positions_fact]    Script Date: 21/09/2017 3:16:27 PM ******/
 SET ANSI_NULLS ON
 GO
+
 SET QUOTED_IDENTIFIER ON
 GO
+
 CREATE TABLE [hris].[non_primary_positions_fact](
-   [non_primary_position_key] [int]   NOT NULL,
-   [portfolio_key] [int] NOT NULL,
-   [service_stream_key] [int] NOT NULL,
-   [program_key] [int] NOT NULL,
-   [position_key] [int] NOT NULL,
-   [employee_key] [int] NOT NULL,
-   [position_type] [varchar] (100) NULL,
-   [position_code] [varchar] (100) NULL,
-   [manager_key] [int] NOT NULL,
-   [manager_position_key] [int] NOT NULL,
-   [contract_expire_date_key] [int]  NULL,
-   [position_effective_date_key] [int]  NULL,
-   [position_end_date_key] [int]  NULL
-)
-GO
-alter table [hris].[non_primary_positions_fact]
-add constraint [pk_non_primary_positions_fact]
-    primary key clustered([non_primary_position_key] asc);
-GO
--- creating foreign key on [portfolio_key] in table [non_primary_positions_fact]
-alter table [hris].[non_primary_positions_fact]
-add constraint [fk_conform_non_primary_positions_fact_portfolio_key]
-    foreign key ([portfolio_key])
-    references [hris].[portfolio_dim]
-        (portfolio_key)
-    on delete no action on update no action;
-go
--- creating foreign key on [program_key] in table [non_primary_positions_fact]
-alter table [hris].[non_primary_positions_fact]
-add constraint [fk_conform_non_primary_positions_fact_program_key]
-    foreign key ([program_key])
-    references [hris].[program_dim]
-        (program_key)
-    on delete no action on update no action;
-go
-
-alter table [hris].[non_primary_positions_fact]
-add constraint [fk_conform_non_primary_positions_fact_service_stream_key]
-    foreign key ([service_stream_key])
-    references [hris].[service_stream_dim]
-        (service_stream_key)
-    on delete no action on update no action;
-go
-
-alter table [hris].[non_primary_positions_fact]
-add constraint [fk_conform_non_primary_positions_fact_position_key]
-    foreign key ([position_key])
-    references [hris].[position_dim]
-        (position_key)
-    on delete no action on update no action;
-go
-
-alter table [hris].[non_primary_positions_fact]
-add constraint [fk_conform_non_primary_positions_fact_manager_position_key]
-    foreign key ([manager_position_key])
-    references [hris].[position_dim]
-        (position_key)
-    on delete no action on update no action;
-go
-
-
-alter table [hris].[non_primary_positions_fact]
-add constraint [fk_conform_non_primary_positions_fact_employee_key]
-    foreign key ([employee_key])
-    references [hris].[employee_dim]
-        (employee_key)
-    on delete no action on update no action;
-go
-
-alter table [hris].[non_primary_positions_fact]
-add constraint [fk_conform_non_primary_positions_fact_manager_key]
-    foreign key ([manager_key])
-    references [hris].[employee_dim]
-        (employee_key)
-    on delete no action on update no action;
-go
-
-alter table [hris].[non_primary_positions_fact]
-add constraint [fk_conform_non_primary_positions_fact_contract_expire_date_key]
-    foreign key ([contract_expire_date_key])
-    references [hris].[date_dim]
-        (date_key)
-    on delete no action on update no action;
-go
-
-alter table [hris].[non_primary_positions_fact]
-add constraint [fk_conform_non_primary_positions_fact_position_effective_date_key]
-    foreign key ([position_effective_date_key])
-    references [hris].[date_dim]
-        (date_key)
-    on delete no action on update no action;
-go
-
-alter table [hris].[non_primary_positions_fact]
-add constraint [fk_conform_non_primary_positions_fact_position_end_date_key]
-    foreign key ([position_end_date_key])
-    references [hris].[date_dim]
-        (date_key)
-    on delete no action on update no action;
-go
-
+	[non_primary_position_key] [int] NOT NULL,
+	[portfolio_key] [int] NOT NULL,
+	[service_stream_key] [int] NOT NULL,
+	[program_key] [int] NOT NULL,
+	[position_key] [int] NOT NULL,
+	[employee_key] [int] NOT NULL,
+	[position_type] [varchar](100) NULL,
+	[position_code] [varchar](100) NULL,
+	[manager_key] [int] NOT NULL,
+	[manager_position_key] [int] NOT NULL,
+	[contract_expire_date_key] [int] NULL,
+	[position_effective_date_key] [int] NULL,
+	[position_end_date_key] [int] NULL,
+	[pay_rate] [decimal](18,6) NULL,
+ CONSTRAINT [pk_non_primary_positions_fact] PRIMARY KEY CLUSTERED 
+(
+	[non_primary_position_key] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
 
 GO
+
+ALTER TABLE [hris].[non_primary_positions_fact]  WITH CHECK ADD  CONSTRAINT [fk_conform_non_primary_positions_fact_contract_expire_date_key] FOREIGN KEY([contract_expire_date_key])
+REFERENCES [hris].[date_dim] ([date_key])
+GO
+
+ALTER TABLE [hris].[non_primary_positions_fact] CHECK CONSTRAINT [fk_conform_non_primary_positions_fact_contract_expire_date_key]
+GO
+
+ALTER TABLE [hris].[non_primary_positions_fact]  WITH CHECK ADD  CONSTRAINT [fk_conform_non_primary_positions_fact_employee_key] FOREIGN KEY([employee_key])
+REFERENCES [hris].[employee_dim] ([employee_key])
+GO
+
+ALTER TABLE [hris].[non_primary_positions_fact] CHECK CONSTRAINT [fk_conform_non_primary_positions_fact_employee_key]
+GO
+
+ALTER TABLE [hris].[non_primary_positions_fact]  WITH CHECK ADD  CONSTRAINT [fk_conform_non_primary_positions_fact_manager_key] FOREIGN KEY([manager_key])
+REFERENCES [hris].[employee_dim] ([employee_key])
+GO
+
+ALTER TABLE [hris].[non_primary_positions_fact] CHECK CONSTRAINT [fk_conform_non_primary_positions_fact_manager_key]
+GO
+
+ALTER TABLE [hris].[non_primary_positions_fact]  WITH CHECK ADD  CONSTRAINT [fk_conform_non_primary_positions_fact_manager_position_key] FOREIGN KEY([manager_position_key])
+REFERENCES [hris].[position_dim] ([position_key])
+GO
+
+ALTER TABLE [hris].[non_primary_positions_fact] CHECK CONSTRAINT [fk_conform_non_primary_positions_fact_manager_position_key]
+GO
+
+ALTER TABLE [hris].[non_primary_positions_fact]  WITH CHECK ADD  CONSTRAINT [fk_conform_non_primary_positions_fact_portfolio_key] FOREIGN KEY([portfolio_key])
+REFERENCES [hris].[portfolio_dim] ([portfolio_key])
+GO
+
+ALTER TABLE [hris].[non_primary_positions_fact] CHECK CONSTRAINT [fk_conform_non_primary_positions_fact_portfolio_key]
+GO
+
+ALTER TABLE [hris].[non_primary_positions_fact]  WITH CHECK ADD  CONSTRAINT [fk_conform_non_primary_positions_fact_position_effective_date_key] FOREIGN KEY([position_effective_date_key])
+REFERENCES [hris].[date_dim] ([date_key])
+GO
+
+ALTER TABLE [hris].[non_primary_positions_fact] CHECK CONSTRAINT [fk_conform_non_primary_positions_fact_position_effective_date_key]
+GO
+
+ALTER TABLE [hris].[non_primary_positions_fact]  WITH CHECK ADD  CONSTRAINT [fk_conform_non_primary_positions_fact_position_end_date_key] FOREIGN KEY([position_end_date_key])
+REFERENCES [hris].[date_dim] ([date_key])
+GO
+
+ALTER TABLE [hris].[non_primary_positions_fact] CHECK CONSTRAINT [fk_conform_non_primary_positions_fact_position_end_date_key]
+GO
+
+ALTER TABLE [hris].[non_primary_positions_fact]  WITH CHECK ADD  CONSTRAINT [fk_conform_non_primary_positions_fact_position_key] FOREIGN KEY([position_key])
+REFERENCES [hris].[position_dim] ([position_key])
+GO
+
+ALTER TABLE [hris].[non_primary_positions_fact] CHECK CONSTRAINT [fk_conform_non_primary_positions_fact_position_key]
+GO
+
+ALTER TABLE [hris].[non_primary_positions_fact]  WITH CHECK ADD  CONSTRAINT [fk_conform_non_primary_positions_fact_program_key] FOREIGN KEY([program_key])
+REFERENCES [hris].[program_dim] ([program_key])
+GO
+
+ALTER TABLE [hris].[non_primary_positions_fact] CHECK CONSTRAINT [fk_conform_non_primary_positions_fact_program_key]
+GO
+
+ALTER TABLE [hris].[non_primary_positions_fact]  WITH CHECK ADD  CONSTRAINT [fk_conform_non_primary_positions_fact_service_stream_key] FOREIGN KEY([service_stream_key])
+REFERENCES [hris].[service_stream_dim] ([service_stream_key])
+GO
+
+ALTER TABLE [hris].[non_primary_positions_fact] CHECK CONSTRAINT [fk_conform_non_primary_positions_fact_service_stream_key]
+GO
+
+
