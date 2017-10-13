27f2549ac0168e82ee86d99a06c9ead2304eb0e5
frank.wang <frank.wang@anglicaresa.com.au>
2017-10-13 13:36:23 +1030

    Add processing table check sql.

--- ReportingWarehouse/SQL_Scripts/MAINTAIN/Check Job History.sql
+++ ReportingWarehouse/SQL_Scripts/MAINTAIN/Check Job History.sql
@@ -15,3 +15,13 @@ select top 1000 H.RUN_DATE,h.message,H.run_status,h.* from dbo.sysjobhistory h i
 		CONVERT(varchar,DATEADD(DAY,-2,GETDATE()), 112),
 		CONVERT(varchar,DATEADD(DAY,-3,GETDATE()), 112)
 	) 
+
+
+use dwhb;
+select t.[database],t.[schema],t.table_key,t.table_name, f.cdc_date_time, s.status_name  from 
+DWHB.management.etl_process_fact f
+INNER JOIN management.table_dim t on f.table_key = t.table_key
+INNER JOIN management.status_dim s on f.status_key = s.status_key
+INNER JOIN management.date_dim d on f.date_key = d.date_key
+and d.display_date = '13/10/2017'
+order by f.cdc_date_time desc
