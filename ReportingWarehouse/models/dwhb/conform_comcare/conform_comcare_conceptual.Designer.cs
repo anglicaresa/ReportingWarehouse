cf87efdad3bdfe69e778b22d61c66dcd7a770f6b
frank.wang <frank.wang@anglicaresa.com.au>
2017-11-15 11:06:35 +1030

    Comcare data model updated

--- ReportingWarehouse/models/dwhb/conform_comcare/conform_comcare_conceptual.Designer.cs
+++ ReportingWarehouse/models/dwhb/conform_comcare/conform_comcare_conceptual.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\projects\ReportingWarehouse\ReportingWarehouse\models\dwhb\conform_comcare\conform_comcare_conceptual.edmx'. 
+﻿// T4 code generation is enabled for model '\\anglicare-sa.org.au\files\frank.wang\My Documents\Source\ReportingWarehouse\ReportingWarehouse\models\dwhb\conform_comcare\conform_comcare_conceptual.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
