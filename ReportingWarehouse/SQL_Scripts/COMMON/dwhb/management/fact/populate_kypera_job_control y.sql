8cc0765d953e34cbd2abd760f312a706acae5ab0
jshen <jshen@anglicaresa.com.au>
2018-02-27 10:18:32 +1030

    update elts and sqls for Kypera fact and extract tables.

---
+++ ReportingWarehouse/SQL_Scripts/COMMON/dwhb/management/fact/populate_kypera_job_control y.sql
@@ -0,0 +1,63 @@
+﻿------------
+-- EXTRACT
+------------
+use dwhb;
+GO
+-- kypera -> DWHB.extract_kypera
+insert into [management].[job_control] 
+values ((select table_key from [management].[table_dim] where table_name = 'Occupancies_Occupancy' and [schema] = 'dbo' and [system] = 'kypera'), 'YES',
+ (select table_key from [management].[table_dim] where table_name = 'Occupancies_Occupancy' and [schema] = 'extract_kypera' and [system] = 'dwh'), 
+ (select package_key from [management].[package_dim] where package_name = 'EXTRACT' and sub_system = 'Extract system'),1,'daily');
+
+insert into [management].[job_control] 
+values ((select table_key from [management].[table_dim] where table_name = 'Occupancies_RentCalculation_Arrears_EffectiveBalances' and [schema] = 'dbo' and [system] = 'kypera'), 'YES',
+ (select table_key from [management].[table_dim] where table_name = 'Occupancies_RentCalculation_Arrears_EffectiveBalances' and [schema] = 'extract_kypera' and [system] = 'dwh'), 
+ (select package_key from [management].[package_dim] where package_name = 'EXTRACT' and sub_system = 'Extract system'),1,'daily');
+
+
+insert into [management].[job_control] 
+values ((select table_key from [management].[table_dim] where table_name = 'PropertyStructure_Block' and [schema] = 'dbo' and [system] = 'kypera'), 'YES',
+ (select table_key from [management].[table_dim] where table_name = 'PropertyStructure_Block' and [schema] = 'extract_kypera' and [system] = 'dwh'), 
+ (select package_key from [management].[package_dim] where package_name = 'EXTRACT' and sub_system = 'Extract system'),1,'daily');
+
+
+insert into [management].[job_control] 
+values ((select table_key from [management].[table_dim] where table_name = 'PropertyStructure_BlockUnit' and [schema] = 'dbo' and [system] = 'kypera'), 'YES',
+ (select table_key from [management].[table_dim] where table_name = 'PropertyStructure_BlockUnit' and [schema] = 'extract_kypera' and [system] = 'dwh'), 
+ (select package_key from [management].[package_dim] where package_name = 'EXTRACT' and sub_system = 'Extract system'),1,'daily');
+
+
+insert into [management].[job_control] 
+values ((select table_key from [management].[table_dim] where table_name = 'PropertyStructure_District' and [schema] = 'dbo' and [system] = 'kypera'), 'YES',
+ (select table_key from [management].[table_dim] where table_name = 'PropertyStructure_District' and [schema] = 'extract_kypera' and [system] = 'dwh'), 
+ (select package_key from [management].[package_dim] where package_name = 'EXTRACT' and sub_system = 'Extract system'),1,'daily');
+
+
+
+ 
+insert into [management].[job_control] 
+values ((select table_key from [management].[table_dim] where table_name = 'PropertyStructure_scheme' and [schema] = 'dbo' and [system] = 'kypera'), 'YES',
+ (select table_key from [management].[table_dim] where table_name = 'PropertyStructure_scheme' and [schema] = 'extract_kypera' and [system] = 'dwh'), 
+ (select package_key from [management].[package_dim] where package_name = 'EXTRACT' and sub_system = 'Extract system'),1,'daily');
+
+insert into [management].[job_control] 
+values ((select table_key from [management].[table_dim] where table_name = 'PropertyStructure_Unit' and [schema] = 'dbo' and [system] = 'kypera'), 'YES',
+ (select table_key from [management].[table_dim] where table_name = 'PropertyStructure_Unit' and [schema] = 'extract_kypera' and [system] = 'dwh'), 
+ (select package_key from [management].[package_dim] where package_name = 'EXTRACT' and sub_system = 'Extract system'),1,'daily');
+
+
+insert into [management].[job_control] 
+values ((select table_key from [management].[table_dim] where table_name = 'Security_user' and [schema] = 'dbo' and [system] = 'kypera'), 'YES',
+ (select table_key from [management].[table_dim] where table_name = 'Security_user' and [schema] = 'extract_kypera' and [system] = 'dwh'), 
+ (select package_key from [management].[package_dim] where package_name = 'EXTRACT' and sub_system = 'Extract system'),1,'daily');
+
+-------------
+-- DELIVERY
+-------------
+-- DWHB.conform_fiannce -> DWHF.finance (Dimensions)
+
+-- DWHB.conform_finnace -> DWHF.finance (Facts)
+insert into [management].[job_control] 
+values ((select table_key from [management].[table_dim] where table_name = 'Kypera_Tenancy_Balance_Fact' and [schema] = 'conform_finance' and [system] = 'dwh'), 
+'YES', (select table_key from [management].[table_dim] where table_name = 'Kypera_Tenancy_Balance_Fact' and [schema] = 'finance' and [system] = 'dwh'), 
+(select package_key from [management].[package_dim] where package_name = 'DELIVERY' and sub_system = 'Delivery system'),2,'daily');
---
+++ ReportingWarehouse/SQL_Scripts/PROD/dwhb/management/dimension/populate_kypera_table_dim.sql
@@ -0,0 +1,34 @@
+﻿-------------------
+-- PROD environment
+-------------------
+use dwhb;
+GO
+-- schema: kypera source data
+INSERT INTO [management].[TABLE_DIM] VALUES ('Occupancies_Occupancy', 'kypera', 'dbo', 'anglicare', 'appsql-3');
+INSERT INTO [management].[TABLE_DIM] VALUES ('Occupancies_RentCalculation_Arrears_EffectiveBalances', 'kypera', 'dbo', 'anglicare', 'appsql-3');
+INSERT INTO [management].[TABLE_DIM] VALUES ('PropertyStructure_Block', 'kypera', 'dbo', 'anglicare', 'appsql-3');
+INSERT INTO [management].[TABLE_DIM] VALUES ('PropertyStructure_BlockUnit', 'kypera', 'dbo', 'anglicare', 'appsql-3');
+INSERT INTO [management].[TABLE_DIM] VALUES ('PropertyStructure_District', 'kypera', 'dbo', 'anglicare', 'appsql-3');
+INSERT INTO [management].[TABLE_DIM] VALUES ('PropertyStructure_scheme', 'kypera', 'dbo', 'anglicare', 'appsql-3');
+INSERT INTO [management].[TABLE_DIM] VALUES ('PropertyStructure_Unit', 'kypera', 'dbo', 'anglicare', 'appsql-3');
+INSERT INTO [management].[TABLE_DIM] VALUES ('Security_user', 'kypera', 'dbo', 'anglicare', 'appsql-3');
+
+-- schema: replicated kypera tables
+INSERT INTO [management].[TABLE_DIM] VALUES ('Occupancies_Occupancy', 'dwh', 'extract_kypera', 'dwhb', 'AZ-SQLBI01');
+INSERT INTO [management].[TABLE_DIM] VALUES ('Occupancies_RentCalculation_Arrears_EffectiveBalances', 'dwh', 'extract_kypera', 'dwhb', 'AZ-SQLBI01');
+INSERT INTO [management].[TABLE_DIM] VALUES ('PropertyStructure_Block', 'dwh', 'extract_kypera', 'dwhb', 'AZ-SQLBI01');
+INSERT INTO [management].[TABLE_DIM] VALUES ('PropertyStructure_BlockUnit', 'dwh', 'extract_kypera', 'dwhb', 'AZ-SQLBI01');
+INSERT INTO [management].[TABLE_DIM] VALUES ('PropertyStructure_District', 'dwh', 'extract_kypera', 'dwhb', 'AZ-SQLBI01');
+INSERT INTO [management].[TABLE_DIM] VALUES ('PropertyStructure_scheme', 'dwh', 'extract_kypera', 'dwhb', 'AZ-SQLBI01');
+INSERT INTO [management].[TABLE_DIM] VALUES ('PropertyStructure_Unit', 'dwh', 'extract_kypera', 'dwhb', 'AZ-SQLBI01');
+INSERT INTO [management].[TABLE_DIM] VALUES ('Security_user', 'dwh', 'extract_kypera', 'dwhb', 'AZ-SQLBI01');
+
+
+-- schema: conform_riskman
+INSERT INTO [management].[TABLE_DIM] VALUES ('Kypera_Tenancy_Balance_Fact', 'dwh', 'conform_finance', 'dwhb', 'AZ-SQLBI01');
+
+
+
+
+-- schema: riskman
+INSERT INTO [management].[TABLE_DIM] VALUES ('Kypera_Tenancy_Balance_Fact', 'dwh', 'finance', 'dwhf_finance', 'AZ-SQLBI01');i 
