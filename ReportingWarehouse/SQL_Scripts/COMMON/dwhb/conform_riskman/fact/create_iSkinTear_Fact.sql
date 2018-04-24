2f91bbab64fc87c3f85ebd6032f9f667caef6b9d
jshen <jshen@anglicaresa.com.au>
2018-04-24 10:53:48 +0930

    update model for riskman to add related tables for skintear_fact.

--- ReportingWarehouse/SQL_Scripts/COMMON/dwhb/conform_riskman/fact/create_iSkinTear_Fact.sql
+++ ReportingWarehouse/SQL_Scripts/COMMON/dwhb/conform_riskman/fact/create_iSkinTear_Fact.sql
@@ -1,6 +1,6 @@
-﻿create table riskman.SkinTear_Fact
+﻿create table conform_riskman.SkinTear_Fact
 (
-ST_key  int ,
+ST_key  int identity(1,1),
 Incident_ID int,
 Location_on_body nvarchar(max),
 Other_location nvarchar(max),
@@ -12,8 +12,7 @@ How_skin_tear_developed nvarchar(max))
 	go
 
 -- PK
-alter table [riskman].[SkinTear_Fact]
+alter table [conform_riskman].[SkinTear_Fact]
 add constraint [pk_SkinTear_Fact]
     primary key clustered ([ST_key] asc);
 go
-
