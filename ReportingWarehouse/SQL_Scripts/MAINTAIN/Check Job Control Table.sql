291e19985d04ed401b531bb7f49c6730555fd4b6
Frank Wang <Frank.Wang@anglicaresa.com.au>
2018-09-06 13:33:37 +0930

    Add UserAccessControl Table.

--- ReportingWarehouse/SQL_Scripts/MAINTAIN/Check Job Control Table.sql
+++ ReportingWarehouse/SQL_Scripts/MAINTAIN/Check Job Control Table.sql
@@ -1,4 +1,4 @@
-use dwhb;
+--use dwhb;
 go
 
  select  t1.job_key,d1.[system], d1.[schema] source_schema, d1.[table_name] source_table, d2.[schema] dest_schema, d2.[table_name] dest_table,  d3.package_name, t1.[priority]
@@ -13,18 +13,16 @@ go
   --order by t1.job_key desc
   order by d1.[system], d1.[schema], t1.[priority], d1.table_name;
 
-  use msdb
+--  use msdb
 
-select H.RUN_DATE,h.message,H.run_status from dbo.sysjobhistory h inner join dbo.sysjobs j
-	on j.job_id = h.job_id and h.run_status <> 1 
-	and run_date IN 
-	(
-		CONVERT(varchar,DATEADD(DAY,-1,GETDATE()), 112),
-		CONVERT(varchar,DATEADD(DAY,-2,GETDATE()), 112),
-		CONVERT(varchar,DATEADD(DAY,-3,GETDATE()), 112)
-	) 
+--select H.RUN_DATE,h.message,H.run_status from dbo.sysjobhistory h inner join dbo.sysjobs j
+--	on j.job_id = h.job_id and h.run_status <> 1 
+--	and run_date IN 
+--	(
+--		CONVERT(varchar,DATEADD(DAY,-1,GETDATE()), 112),
+--		CONVERT(varchar,DATEADD(DAY,-2,GETDATE()), 112),
+--		CONVERT(varchar,DATEADD(DAY,-3,GETDATE()), 112)
+--	) 
 
-use dwhb;
-update  management.job_control 
-set priority = 1 where job_key = 226;
+
 
